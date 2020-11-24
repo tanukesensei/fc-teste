461a3fbaabe647ca45f3b61fd095aa8c350417cc
tanukesensei <luanvs.hack@gmail.com>
2020-11-24 01:26:42 -0300

    first commit

---
+++ htdocs/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="src/view/cadastro_medico.php">Cadastro de Médico</a>
+    <h3>Lista de Atendimento</h3>   
+</body>
+</html>
+
+<?php
+# phpinfo();
+?>
